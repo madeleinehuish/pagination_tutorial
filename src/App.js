62943db1ec83bd56a093a0806d903267ce5ff71a
Madeleine Huish <madeleinehuish@gmail.com>
2019-02-23 09:57:50 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     this.setState({ currentPage, currentCountries, totalPages });
   }
 
+
   onPageChanged = data => {
    const { currentPage, totalPages, pageLimit } = data;
 
